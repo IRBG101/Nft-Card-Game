f796c2873ed3cc37366b2a3230897ed96858f793
RissyG101 <125858799+IRBG101@users.noreply.github.com>
2023-08-13 21:42:13 -0500

    A NFT Card Game

---
+++ client/src/contract/index.js
@@ -0,0 +1,7 @@
+import contract from './AVAXGods.json';
+
+export const ADDRESS = '0xc0a5e587F71f8a18dA4Aa48154AF3F37C8158264';
+
+export const { abi: ABI } = contract;
+// ABI allows the frontend side to call the function created inside of the smartcontract
+
---
+++ client/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import {CreateBattle, Home, JoinBattle, Battle, Battleground } from './page';
+import { OnboardModal } from './components';
+import './index.css';
+import { GlobalContextProvider } from './context';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <GlobalContextProvider>
+    <OnboardModal />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/create-battle" element={<CreateBattle />} />
+      <Route path="/join-battle" element={<JoinBattle />} />
+      <Route path="/battleground" element={<Battleground />} />
+      <Route path="/battle/:battleName" element={<Battle />} />
+    </Routes>
+  </GlobalContextProvider>
+  </BrowserRouter>,
+);
